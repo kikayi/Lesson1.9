b0e8c568066c17bb138654e2be0ebf6d0750eba6
Rachelle Anne <61951624+kikayi@users.noreply.github.com>
2020-03-14 15:44:19 -0400

    Add project files.

---
+++ Lesson1.9.cpp
@@ -0,0 +1,16 @@
+//Rachelle Anne 
+//2/26/2020
+//2/26/2020
+//This program is a reflection of 1.9 to 1.??'s lesson.
+
+#include <iostream>
+
+int main()
+{
+	int x = 3;
+	x = x + 1;
+	std::cout << x << std::endl;
+	std::cout << x;
+
+	return 0;
+}
